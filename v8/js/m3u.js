055090c729c42572a9c59796a181dc2f95301512
TV365-VN <162657802+TV365-VN@users.noreply.github.com>
2026-07-24 01:12:19 +0700

    Update m3u.js

--- v8/js/m3u.js
+++ v8/js/m3u.js
@@ -1,1 +1,84 @@
+const M3U_URL = atob(
+    "aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL1RWMzY1LVZOL1RWMzY1LURBVEEvbWFpbi9lcnJvci5tM3U="
+);
 
+let channels = [];
+let categories = [];
+
+async function loadM3U() {
+    try {
+        const response = await fetch(M3U_URL, {
+            cache: "no-cache"
+        });
+
+        const text = await response.text();
+
+        parseM3U(text);
+
+    } catch (e) {
+
+        console.error("Không tải được M3U", e);
+
+    }
+}
+
+function parseM3U(text) {
+
+    channels = [];
+    categories = ["TẤT CẢ"];
+
+    const lines = text.split("\n");
+
+    let current = {};
+
+    for (let line of lines) {
+
+        line = line.trim();
+
+        if (line.startsWith("#EXTINF")) {
+
+            const name =
+                line.split(",").pop().trim();
+
+            const logo =
+                (
+                    line.match(/tvg-logo="(.*?)"/) || []
+                )[1] || "";
+
+            const group =
+                (
+                    line.match(/group-title="(.*?)"/) || []
+                )[1] || "KHÁC";
+
+            current = {
+
+                name,
+
+                logo,
+
+                group
+
+            };
+
+            if (!categories.includes(group)) {
+
+                categories.push(group);
+
+            }
+
+        }
+
+        else if (
+            line &&
+            !line.startsWith("#")
+        ) {
+
+            current.url = line;
+
+            channels.push(current);
+
+            current = {};
+
+        }
+    }
+}
